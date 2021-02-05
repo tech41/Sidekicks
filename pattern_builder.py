2a7c9725ee6d989a51389f4a3490a3256b3151dc
jncunningham42 <66345289+jncunningham42@users.noreply.github.com>
2021-02-05 00:09:54 -0800

    Add 'Square' button subclass

--- pattern_builder.py
+++ pattern_builder.py
@@ -1,19 +1,23 @@
 # pattern_builder: Tool for creating spell patterns and reading/writing to the Sidekicks CSV
 import tkinter as tk
 
-#class Square(tk.Button):
-    #def __init__(self, master):
-        #self.config(bg = "blue")
+class Square(tk.Button):
+    def __init__(self, master):
+        tk.Button.__init__(self, master)
+        self.config(height = 2, width = 5)
+        self.config(bg = "blue")
 
 class PatternBuilderTool:
     def __init__(self, master):
         self.master = master
         master.title("Sidekicks: Pattern Builder")
+        master.geometry('1000x500')
 
         self.label = tk.Label(master, text="Simple pattern")
         self.label.pack()
         
-        self.square = tk.Button(master, height = 2, width = 5)
+        #self.square = tk.Button(master, height = 2, width = 5)
+        self.square = Square(master)
         self.square.config(bg = "blue")
         self.square.pack()
 
