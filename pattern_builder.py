8a3bf4edb38ff752715ab609015a03597eaebc3f
jncunningham42 <66345289+jncunningham42@users.noreply.github.com>
2021-02-28 18:21:51 -0800

    Clean up

--- pattern_builder.py
+++ pattern_builder.py
@@ -13,8 +13,7 @@ class Square(tk.Button):
         self.grid(row = myRow, column = myCol)
 
     def update_color(self, color):
-        #print(self)
-        #print(color)
+        self.color = color
         self.config(bg = color)
 
     def click_change_color(self):
@@ -67,7 +66,6 @@ class PatternBuilderTool:
         master.colorList = ["white", "green", "blue", "yellow"] # first color = blank/none
         
         cardData = pandas.read_csv('sidekicksDeck.csv', index_col = 'ID')
-        #print(cardData["Name"])        
 
         self.label = tk.Label(master, text="Simple pattern")
         self.label.grid(row = 1, column = 1)
@@ -84,14 +82,9 @@ class PatternBuilderTool:
         self.patternLvl1 = PatternFrame(master, 2, 3)
         self.spacer2 = Spacer(master, 2, 4)
         self.patternLvl2 = PatternFrame(master, 2, 5)
-        
-        ## debugging
-        #s = self.patternLvl1.squareMat[0][2]
-        #print(s)
-        #s.update_color("green")        
-        
+    
     def parse_pattern_col(self, patternStr):
-        #print("---------- Pattern string is ", patternStr)
+
         dots = [m.start() for m in re.finditer('\.', patternStr)]
         if len(dots) > 0:
             rIdx = [dot - 1 for dot in dots]
@@ -102,8 +95,7 @@ class PatternBuilderTool:
         else:
             rows = []
             cols = []
-            
-        #print([rows, cols])
+
         return [rows, cols]
     
     def select_card(self, event):
@@ -116,66 +108,23 @@ class PatternBuilderTool:
         
         patternLvlList = [1, 2]
         colorList = ["green", "blue", "yellow"] # need to use list from root
-        #colorList = self.patternLvl1.square.colorList
 
-        
-        
-        # Possibly use a dict instead:
-        # See: https://stackoverflow.com/questions/26164568/how-can-i-dynamically-refer-to-a-variable-in-python/26164638
-        
-        #print(thisSquareMat)
         for lvl in patternLvlList:
             
             thisPattern = getattr(self, 'patternLvl%s' % (lvl))
             thisPattern.blank_out_squares()
 
             thisSquareMat = thisPattern.squareMat
-            #print(thisSquareMat)
-            
-            ## debugging
-            #s = self.patternLvl1.squareMat[0][2]
-            #print(s)
-            #s.update_color("green") 
-            
+           
             for color in colorList:
                 colName = "Pattern_%s_%s" % (lvl, color)
                 thisPattern =  cardData[colName][csvIdx+1]
                 rowsCols = self.parse_pattern_col(thisPattern)
                 rows = rowsCols[0]
                 cols = rowsCols[1]
-                #print(rows)
-                #print(cols)
                 for r, c in zip(rows, cols):
-                    #print(r)
-                    #print(c)
                     s = thisSquareMat[r][c]
-                    #print(s)
                     s.update_color(color)
-                    a=1
-
-        
-        
-
-        
-        ## read CSV pattern columns for index
-        #pat1g = cardData["Pattern_1_g"][cardIdx[0]]
-        #n = [m.start() for m in re.finditer('_', pat1g)] # doesn't work yet. See https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
-        #print(n)
-        ##print(p1g)
-        
-        
-        # get all columns with title Pattern
-        patternCols = cardData.filter(regex="Pattern", axis=1)
-        #print(patternCols)
-
-        
-        # convert nanDeck format to python
-        
-        # update strength text according to CSV (counting how many grids of that type)
-        
-        # update grid colors according to CSV
-        
-        
         
 root = tk.Tk()
 patternBuilder = PatternBuilderTool(root)
